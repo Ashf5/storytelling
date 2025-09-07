c31e4981f78af7227ccc2772319448ac8a56cac1
Ashf5 <friedash85@gmail.com>
2025-09-07 18:08:34 +0300

    add another change to cors

--- server/app.js
+++ server/app.js
@@ -10,13 +10,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 app.use(
   cors({
-    origin: ['http://localhost:5173/'],
-    credentials: true, // ✅ allow cookies
+    origin: ['http://localhost:5173'],
+    credentials: true, 
   })
 )
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
 
 app.use(express.json());
 app.use(cookieParser());
