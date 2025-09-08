ffe4d9bac72c91cade7bda27e329e897f7b07548
Ashf5 <friedash85@gmail.com>
2025-09-08 17:48:44 +0300

    add basic routing...

--- client/src/app/pages/Homepage.tsx
+++ client/src/app/pages/Homepage.tsx
@@ -1,15 +1,25 @@
 
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import StoryList from "../components/StoryList";
 import { useAppSelector } from "../features/store";
+import { useEffect } from "react";
 
 
 const Homepage: React.FC = () => {
 
+    const navigate = useNavigate();
     const token = useAppSelector((state) => state.user.accessToken);
 
+    // use effect to redirect if no token
+    useEffect(() => {
+        if (token === null) navigate('/login');
+    }, [token, navigate]);
+    
+    if (token === null) return null;
+
     return (
+        
         <div>
             <Link to={'/stories'}>Create Story</Link>
             <h3>Stories</h3>
