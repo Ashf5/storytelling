a6cd83fda077702bb57886f007bfc4808c93d855
Ashf5 <friedash85@gmail.com>
2025-09-10 16:53:00 +0300

    maybe found the bug?

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -23,7 +23,7 @@ export async function register(req, res) {
         let user = await registerDB(username, email, password_hash);
         if (!user) throw new Error('An error occured');
         console.log(user)
-        const {accessToken, refreshToken} = createTokenAndRefresh(email, user.id);
+        const {accessToken, refreshToken} = createTokenAndRefresh(email, user[0].id);
         res.cookie("refreshToken", refreshToken, {httpOnly: true, secure: true});
         return res.status(201).json({msg: 'success', accessToken});
     } 
